44941db0507a0ca1fcb95128de83a900b9be2ca1
Indranil Dutta <duttaindranil497@gmail.com>
2016-04-19 02:05:25 +0530

    Added get_internals

--- main.py
+++ main.py
@@ -29,8 +29,10 @@ class Semester(object):
     attendance_absent_id = attendance_id + 'classesAbsent_'
     attendance_percent_id = attendance_id + 'attendancePercentage_'
     attendance_last_updated = attendance_id + 'lastUpdatedStamp_'
-    internal_code_id = 'cc_ListAssessmentScores_internalName_'
-    internal_marks_id = 'cc_ListAssessmentScores_obtainedMarks_'
+    internal_id = 'cc_ListAssessmentScores_'
+    internal_code_id = internal_id + 'internalName_'
+    internal_subject_name = internal_id + 'productName_'
+    internal_marks_id = internal_id + 'obtainedMarks_'
 
     #HTML IDs to be used in get_personal_details
     user_name_id = 'cc_ProfileTitle_name'
@@ -182,6 +184,39 @@ class Semester(object):
 
         return user_details
 
+    def get_internals(self):
+        """docstring for get_internals"""
+        soup = self.soup_detailed
+        all_data = {}
+        internals = []
+        position = []
+        for i in '123':
+            pos = str(soup.text.find('Internal Assessment (IA) - ['+i))
+            if pos == -1:
+                continue
+            position.append(pos + 'ia' + i)
+        position.sort()
+        i = 1
+        while True:
+            i_str = str(i)
+            if soup.find('span', {'id' : self.internal_code_id + i_str})!=None:
+                internal_details = {}
+                subject_code = soup.find('span', {'id' : Semester.internal_code_id + i_str})
+                subject_name = soup.find('span', {'id' : Semester.internal_subject_name + i_str})
+                marks = soup.find_all('span', {'id' : Semester.internal_marks_id + i_str})
+
+                internal_details['subject_name'] = str(subject_name.text)
+                for j in range(len(marks)):
+                    internal_details['sessional'+position[j][-1]] = str(marks[j].text)
+                internals.append(internal_details)
+                i = i + 1
+            else:
+                break
+        self.all_data['internals'] = internals
+        all_data['internals'] = internals
+        return all_data
+
+
 if __name__ == '__main__':
     url = "http://websismit.manipal.edu/websis/control/createAnonSession"
     payload = {}
@@ -192,17 +227,24 @@ if __name__ == '__main__':
     sem_details = {}
     attendance_details = {}
     user_details = {}
+    internal_details = {}
     if response.text.find('Profile of') == -1:
         print "Invalid details"
     else:
-        user_details = Semester(session, str(1)).get_personal_details()
-        for i in range(1, 1+8):
+        sem_obj = Semester(session, str(1))
+        user_details = sem_obj.get_personal_details()
+        sem_details = sem_obj.get_data()
+        attendance_details = sem_obj.get_attendance()
+        internal_details = sem_obj.get_internals()
+        """for i in range(1, 1+8):
             try:
                 sem_obj = Semester(session, str(i))
                 sem_details['semester'+str(i)] = sem_obj.get_data()
                 attendance_details['semester' + str(i)] =  sem_obj.get_attendance()
+                attendance_details['semester' + str(i)] = sem_obj.get_personal_details()
+                internal_details['semester' + str(i)] = sem_obj.get_internals()
             except AttributeError:
-                break
+                break"""
 
     user_details_json = json.dumps(user_details, indent=4, sort_keys=True, ensure_ascii=False)
     f = open('user_details.json', 'w')
@@ -217,4 +259,9 @@ if __name__ == '__main__':
     details_json = json.dumps(sem_details, indent=4, sort_keys=True, ensure_ascii=False)
     f = open('data.json', 'w')
     f.write(str(details_json))
+    f.close()
+
+    internals_json = json.dumps(internal_details, indent=4, sort_keys=True, ensure_ascii=False)
+    f = open('internal.json', 'w')
+    f.write(str(internals_json))
     f.close()
